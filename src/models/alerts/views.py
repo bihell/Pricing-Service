a3a3c8fcc3dd39385f0834666e0d0774ff9f75ec
Haseo Chen <tpxcer@outlook.com>
2018-02-18 23:01:52 +0800

    Creating a button to add new alerts with Bootstrap and Jinja

--- src/models/alerts/views.py
+++ src/models/alerts/views.py
@@ -1,16 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session
 
 from src.models.alerts.alert import Alert
+from src.models.items.item import Item
 
 alert_blueprint = Blueprint('alerts', __name__)
+
 
 @alert_blueprint.route('/')
 def index():
     return "This is the alerts index"
 
-@alert_blueprint.route('/new', methods=['POST'])
+
+@alert_blueprint.route('/new', methods=['GET', 'POST'])
+# @user_decorators.requires_login
 def create_alert():
-    pass
+    if request.method == 'POST':
+        name = request.form['name']
+        url = request.form['url']
+        price_limit = float(request.form['price_limit'])
+
+        item = Item(name, url)
+        item.save_to_mongo()
+
+        alert = Alert(session['email'], price_limit, item._id)
+        alert.load_item_price()  # This already saves to MongoDB
+
+    # What happens if it's a GET request
+    return render_template("alerts/new_alert.jinja2")  # Send the user an error if their login was invalid
 
 
 @alert_blueprint.route('/deactivate/<string:alert_id>')
@@ -22,6 +38,7 @@ def deactivate_alert(alert_id):
 # @user_decorators.requires_login
 def get_alert_page(alert_id):
     return render_template('alerts/alert.jinja2', alert=Alert.find_by_id(alert_id))
+
 
 @alert_blueprint.route('/for_user/<string:user_id>')
 def get_alerts_for_user(user_id):
