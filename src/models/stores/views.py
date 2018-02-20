d0f54241397e39b3405235d2d73b4e1da0aa10e1
Haseo Chen <tpxcer@outlook.com>
2018-02-20 15:37:22 +0800

    Creating the store index

--- src/models/stores/views.py
+++ src/models/stores/views.py
@@ -1,11 +1,63 @@
-from flask import Blueprint
+import json
 
-store_blueprint = Blueprint('stores',__name__)
+from flask import Blueprint, render_template, request, redirect, url_for
 
-@store_blueprint.route('/store/<string:name>')
-def store_page():
-    pass
+from src.models.stores.store import Store
+
+store_blueprint = Blueprint('stores', __name__)
+
 
 @store_blueprint.route('/')
 def index():
-    return "This is the stores index"
+    # stores = Store.all()
+    return render_template('stores/store_index.jinja2')
+    # return render_template('stores/store_index.jinja2', stores=stores)
+
+
+
+@store_blueprint.route('/new', methods=['GET', 'POST'])
+def create_store():
+    # if request.method == 'POST':
+    #     name = request.form['name']
+    #     url_prefix = request.form['url_prefix']
+    #     tag_name = request.form['tag_name']
+    #     query = json.loads(request.form['query'])
+    #
+    #     Store(name, url_prefix, tag_name, query).save_to_mongo()
+    #
+    # # What happens if it's a GET request
+    # return render_template("stores/new_store.jinja2")
+    return "This is the store creation page"
+
+
+@store_blueprint.route('/edit/<string:store_id>', methods=['GET', 'POST'])
+def edit_store(store_id):
+    if request.method == 'POST':
+        name = request.form['name']
+        url_prefix = request.form['url_prefix']
+        tag_name = request.form['tag_name']
+        query = json.loads(request.form['query'])
+
+        store = Store.get_by_id(store_id)
+
+        store.name = name
+        store.url_prefix = url_prefix
+        store.tag_name = tag_name
+        store.query = query
+
+        store.save_to_mongo()
+
+        return redirect(url_for('.index'))
+
+    # What happens if it's a GET request
+    return render_template("stores/edit_store.jinja2", store=Store.get_by_id(store_id))
+
+
+@store_blueprint.route('/delete/<string:store_id>')
+def delete_store(store_id):
+    Store.get_by_id(store_id).delete()
+
+
+@store_blueprint.route('/<string:store_id>')
+def store_page(store_id):
+    return render_template('stores/store.jinja2', store=Store.get_by_id(store_id))
