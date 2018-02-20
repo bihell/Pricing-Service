8b45f0a7aac095848130a82c8a6e14a467007656
Haseo Chen <tpxcer@outlook.com>
2018-02-20 14:18:40 +0800

    Deactivating an alert

--- src/models/alerts/alert.py
+++ src/models/alerts/alert.py
@@ -11,12 +11,13 @@ from src.models.items.item import Item
 
 
 class Alert(object):
-    def __init__(self, user_email, price_limit, item_id, _id=None, last_checked=None):
+    def __init__(self, user_email, price_limit, item_id, active=True, _id=None, last_checked=None):
         self.user_email = user_email
         self.price_limit = price_limit
         self.item = Item.get_by_id(item_id)
         self.last_checked = datetime.datetime.utcnow() if last_checked is None else last_checked
         self._id = uuid.uuid4().hex if _id is None else _id
+        self.active = active
 
     def __repr__(self):
         return "<Alert for {} on item {} with price {}>".format(self.user_email.email, self.item.name, self.price_limit)
@@ -56,7 +57,8 @@ class Alert(object):
             "price_limit": self.price_limit,
             "last_checked": self.last_checked,
             "user_email": self.user_email,
-            "item_id": self.item._id
+            "item_id": self.item._id,
+            "active": self.active
         }
         # "active": self.active
 
@@ -78,3 +80,14 @@ class Alert(object):
     @classmethod
     def find_by_id(cls, alert_id):
         return cls(**Database.find_one(AlertConstants.COLLECTION, {'_id': alert_id}))
+
+    def deactivate(self):
+        self.active = False
+        self.save_to_mongo()
+
+    def activate(self):
+        self.active = True
+        self.save_to_mongo()
+
+    def delete(self):
+        Database.remove(AlertConstants.COLLECTION, {'_id': self._id})
